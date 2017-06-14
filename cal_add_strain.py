45dc61ee0c2d8c8e91c32e9e5b440daef760b1b0
chaomy <chaomy@umich.edu>
2017-06-14 09:25:34 -0700

    add readme.txt

---
+++ cal_add_strain.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+###################################################################
+#
+# File Name : cal_add_strain.py
+#
+###################################################################
+#
+# Purpose :  add strain (shear or tensile) to the supercell
+#
+# Creation Date :
+# Last Modified :
+# Created By    : Chaoming Yang
+#
+###################################################################
+
+import numpy as np
+
+
+class cal_add_strain(object):
+
+    def __init__(self):
+
+        return
+
+    def _volume_conserving_ortho_strain(self, delta):
+        atom_number, supercell_base, comment, atom_position =\
+            self.read_vasp_poscar()
+        OrthoM = np.mat([[1 + delta, 0, 0],
+                         [0, 1 - delta, 0],
+                         [0, 0, 1 / (1 - delta**2)]])
+        Transformed_Base = OrthoM * supercell_base
+        with open("POSCAR", 'w') as fid:
+            fid.write("# Screw Bcc\n")
+            fid.write("%12.6f\n" % (self.lattice_constant))
+            fid.write("%12.6f %12.6f %12.6f\n" %
+                      (Transformed_Base[0, 0],
+                       Transformed_Base[0, 1],
+                       Transformed_Base[0, 2]))
+            fid.write("%12.6f %12.6f %12.6f\n" %
+                      (Transformed_Base[1, 0],
+                       Transformed_Base[1, 1],
+                       Transformed_Base[1, 2]))
+            fid.write("%12.6f %12.6f %12.6f\n" %
+                      (Transformed_Base[2, 0], Transformed_Base[2, 1], Transformed_Base[2, 2]))
+            fid.write("%d\n" % (atom_number))
+            fid.write(comment)
+            for i in range(atom_number):
+                fid.write("%12.6f %12.6f %12.6f\n" %
+                          (atom_position[0, i], atom_position[1, i], atom_position[2, i]))
+            fid.close()
+        return
+
+    def _add_volumeric_strain(self, atoms, delta):
+        cell = np.mat(atoms.get_cell())
+        positions = np.mat(atoms.get_positions())
+
+        strain = np.mat([[1 + delta, 0, 0],
+                         [0, 1 + delta, 0],
+                         [0, 0, 1 + delta]], "float")
+        cell = strain * cell
+        positions = positions * strain
+
+        atoms.set_positions(positions)
+        atoms.set_cell(cell)
+        return atoms
+
+    def _volume_conserving_ortho_strain_atoms(self, delta, atoms):
+        strain = np.mat([[1 + delta, 0, 0],
+                         [0, 1 - delta, 0],
+                         [0, 0, 1 / (1 - delta**2)]])
+
+        cell = atoms.get_cell()
+        pos = np.mat(atoms.get_positions())
+
+        cell = strain * cell
+        pos = pos * strain
+        atoms.set_positions(pos)
+        atoms.set_cell(cell)
+        return atoms
+
+    def _volume_conserving_mono_strain_atoms(self, delta, atoms):
+        strain = np.mat([[1, delta, 0],
+                         [delta, 1, 0],
+                         [0, 0, 1 / (1 - delta**2)]])
+        cell = atoms.get_cell()
+        pos = np.mat(atoms.get_positions())
+
+        cell = strain * cell
+        pos = pos * strain
+
+        atoms.set_positions(pos)
+        atoms.set_cell(cell)
+        return atoms
+
+    def _volume_conserving_mono_strain(self, delta):
+        atom_number, supercell_base, comment, atom_position = self.read_vasp_poscar()
+        OrthoM = np.mat([[1, delta, 0],
+                         [delta, 1, 0],
+                         [0, 0, 1 / (1 - delta**2)]])
+        Transformed_Base = OrthoM * supercell_base
+        with open("POSCAR", 'w') as fid:
+            fid.write("# Screw Bcc\n")
+            fid.write("%12.6f\n" % (self.lattice_constant))
+            fid.write("%12.6f %12.6f %12.6f\n" %
+                      (Transformed_Base[0, 0], Transformed_Base[0, 1], Transformed_Base[0, 2]))
+            fid.write("%12.6f %12.6f %12.6f\n" %
+                      (Transformed_Base[1, 0], Transformed_Base[1, 1], Transformed_Base[1, 2]))
+            fid.write("%12.6f %12.6f %12.6f\n" %
+                      (Transformed_Base[2, 0], Transformed_Base[2, 1], Transformed_Base[2, 2]))
+            fid.write("%d\n" % (atom_number))
+            fid.write(comment)
+            for i in range(atom_number):
+                fid.write("%12.6f %12.6f %12.6f\n" %
+                          (atom_position[0, i], atom_position[1, i], atom_position[2, i]))
+            fid.close()
+        return
