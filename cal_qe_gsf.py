ebbea2cdc06b1e80074c2deac32dd7b12f456f88
chaomy <chaomy@umich.edu>
2017-07-28 20:53:07 -0700

    w

--- cal_qe_gsf.py
+++ cal_qe_gsf.py
@@ -3,7 +3,7 @@
 # @Author: chaomy
 # @Date:   2017-06-28 00:35:14
 # @Last Modified by:   chaomy
-# @Last Modified time: 2017-07-28 20:52:24
+# @Last Modified time: 2017-07-28 20:52:54
 
 
 from optparse import OptionParser
@@ -129,7 +129,7 @@ class cal_gsf(gn_config.bcc,
         self.set_nnodes(2)
         self.set_ppn(12)
         self.set_job_title("{}".format(dirname))
-        self.set_wall_time(8)
+        self.set_wall_time(4)
         self.set_main_job("""mpirun pw.x < qe.in > qe.out""")
         self.write_pbs(od=False)
         return
