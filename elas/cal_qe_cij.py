cc1f3688335ce8cad74f9b686bb685ca49c99bb9
chaomy <chaomy@umich.edu>
2017-11-08 21:10:42 -0500

    w

--- elas/cal_qe_cij.py
+++ elas/cal_qe_cij.py
@@ -3,7 +3,7 @@
 # @Author: chaomy
 # @Date:   2017-06-25 14:28:58
 # @Last Modified by:   chaomy
-# @Last Modified time: 2017-11-08 21:10:27
+# @Last Modified time: 2017-11-08 21:10:39
 
 
 from optparse import OptionParser
@@ -120,8 +120,8 @@ class cal_cij(gn_config.bcc,
     def gn_qe_cij_infile(self, atoms):
         self.set_thr('1.0D-6')
         self.set_ecut('40')
-        self.set_kpnts((39, 39, 39))
-        # self.set_kpnts((19, 19, 19))
+        # self.set_kpnts((39, 39, 39))
+        self.set_kpnts((19, 19, 19))
         self.set_degauss('0.03D0')
         with open('qe.in', 'w') as fid:
             fid = self.qe_write_control(fid, atoms)
