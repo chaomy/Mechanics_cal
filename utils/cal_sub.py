e303de1e2ddc487c35a7ffb9ee9145235810a6bf
chaomy <chaomy@umich.edu>
2018-11-09 01:18:37 -0500

    w

--- utils/cal_sub.py
+++ utils/cal_sub.py
@@ -4,7 +4,7 @@
 # @Author: yang37
 # @Date:   2017-06-12 17:03:43
 # @Last Modified by:   chaomy
-# @Last Modified time: 2018-11-09 01:17:24
+# @Last Modified time: 2018-11-09 01:18:31
 
 
 import os
@@ -19,8 +19,8 @@ class subjobs(object):
         self.get_dirs()
 
     def get_dirs(self):
-        self.diriter = iter(glob.glob('dir[-_]*')) 
-        # self.diriter = iter(glob.glob('DIR*'))
+        # self.diriter = iter(glob.glob('dir[-_]*')) 
+        self.diriter = iter(glob.glob('1100_*'))
         # self.diriter = iter(glob.glob("FCC_*"))
 
     def trans_to(self):
